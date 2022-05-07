773d5dcc3c84d438b7213c37dac956099e1ad0a3
Lee <lee@asgsolutions.co.za>
2022-05-07 22:28:19 +0200

    main * Update application to get a list of users/monsters using the JS fetch   API * update state with list of users/monsters

--- src/App.js
+++ src/App.js
@@ -7,36 +7,29 @@ class App extends Component {
         super(props);
 
         this.state = {
-            name: {firstName: 'Lee', lastName: 'Aaron'},
-            company: 'ASG',
+            monsters: [],
         };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState(() => {
+                return {monsters: users}
+            }, () => {
+                console.log(this.state)
+            }))
+        ;
     }
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Hi {this.state.name['firstName']} {this.state.name['lastName']}, I work
-                        at {this.state.company}
-                    </p>
-                    <button onClick={
-                        () => {
-                            this.setState(
-                                () => {
-                                    return {
-                                        name: {firstName: 'Barry', lastName: 'Baeta'}
-                                    }
-                                },
-                                () => {
-                                    console.log(this.state);
-                                })
-                        }
-                    }>
-                        Change Name
-                    </button>
-                </header>
+                {
+                    this.state.monsters.map((monster) => {
+                        return <div key={monster.id}><h1>{monster.name}</h1></div>;
+                })
+                }
             </div>
         );
     }
