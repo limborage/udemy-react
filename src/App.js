e763f4429ff2ee912ebf4e1f9e03693b41b06ab2
Lee <lee@asgsolutions.co.za>
2022-05-07 23:10:36 +0200

    main * Create my own version of filtering out monsters

--- src/App.js
+++ src/App.js
@@ -8,27 +8,48 @@ class App extends Component {
 
         this.state = {
             monsters: [],
+            data: []
         };
+        console.log('constructor');
     }
 
     componentDidMount() {
+        console.log('componentDidMount');
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState(() => {
-                return {monsters: users}
-            }, () => {
-                console.log(this.state)
-            }))
+            .then(users => this.setState(
+                () => {
+                    return {monsters: users, data: users}
+                },
+                () => {
+                    console.log(this.state)
+                }))
         ;
     }
 
     render() {
+        console.log('render');
         return (
             <div className="App">
+                <input className={'search-box'}
+                       type={'search'}
+                       placeholder={'Search for Monster(s)'}
+                       onChange={(event) => {
+                           console.log(event.target.value);
+                           this.setState(() => {
+                               let data = this.state.monsters.filter((monster) => {
+                                   return monster.name.includes(event.target.value);
+                               });
+
+                               return {data}
+                           });
+                           console.log()
+                       }}
+                />
                 {
-                    this.state.monsters.map((monster) => {
+                    this.state.data.map((monster) => {
                         return <div key={monster.id}><h1>{monster.name}</h1></div>;
-                })
+                    })
                 }
             </div>
         );
